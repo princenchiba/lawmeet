c6d06cbbc445a828d147587bfbc365f9d874aa1c
Prince Nchiba <65627985+princenchiba@users.noreply.github.com>
2023-04-24 13:42:26 +0100

    Add files via upload

--- src/routes/CreateReport.tsx
+++ src/routes/CreateReport.tsx
@@ -13,14 +13,21 @@ export default function CreateReport() {
         sections: {title: string, answers: string[]}[]
     }
 
+    interface Qna {
+        question: string,
+        answer: string,
+    }
+
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
     const [currentSectionIndex, setCurrentSectionIndex] = useState(0)
 
-    const [response, setResponse] = useState<Response>({title:'new response', sections: []})
+    const [qnaSections, setQnaSections] = useState<Qna[][]>()
 
     const [currentAnswer, setCurrentAnswer] = useState('')
 
     const [readyToGenerateReport, setReadyToGenerateReport] = useState(false)
+
+    const [reportName, setReportName] = useState('')
 
 
     const location = useLocation();
@@ -29,33 +36,35 @@ export default function CreateReport() {
     const sections = template.sections
 
     useEffect(()=>{
+        setQnaSections(loadQuestionsIntoSeparateArray()) 
     }, [])
 
+    const loadQuestionsIntoSeparateArray =()=>{
+        let qnaSections = []
+        for (let i=0; i<sections.length; i++ ){
+            const section = sections[i]
+            let qnas = []
+            for (let j=0; j<section.questions.length; j++){
+                const qna: Qna = {question: section.questions[j], answer: ''}
+                qnas.push(qna)
+            }
+            qnaSections.push(qnas)
+        }
+        return qnaSections
+    }
+
     const toNextQuestion =()=>{
-        if (currentAnswer !== ''){
-            repondToQuestion()
-            moveFoward()
-            setCurrentAnswer('')
-        }
-        
+        answerCurrentQuestion()
+        moveFoward()
     }
 
-    const repondToQuestion =()=>{
-        addAnswer()
-    }
-
-    const addAnswer =()=>{
-        let res = response
-        console.log('old res', res)
-        if (res.sections[currentSectionIndex] === undefined){
-            let answers = [currentAnswer]
-            res.sections.push({title: sections[currentSectionIndex].title,answers})
-        } else{
-            res.sections[currentSectionIndex].answers.push(currentAnswer)
+    const answerCurrentQuestion =()=>{
+        if (qnaSections !== undefined){
+            const sections = qnaSections
+            // update the answers to the current question
+            sections[currentSectionIndex][currentQuestionIndex].answer = currentAnswer
+            setQnaSections(sections)
         }
-       
-        setResponse(res)
-        console.log('new res',res)
     }
 
     const moveFoward =()=>{
@@ -64,37 +73,37 @@ export default function CreateReport() {
         } else{
             if (currentQuestionIndex + 1 < sections[currentSectionIndex].questions.length){
                 setCurrentQuestionIndex(currentQuestionIndex + 1)
+                if (qnaSections !== undefined){
+                    setCurrentAnswer(qnaSections[currentSectionIndex][currentQuestionIndex + 1].answer)
+                }
             } else if (currentSectionIndex + 1 < sections.length){
                 setCurrentSectionIndex(currentSectionIndex + 1)
                 setCurrentQuestionIndex(0)
+                if (qnaSections !== undefined){
+                    setCurrentAnswer(qnaSections[currentSectionIndex+1][0].answer)
+                }
             }
         }
-       
-    }
-
-    const handleFowardArrowCLick =()=>{
-        if (sections[currentSectionIndex].optional){
-            moveFoward
-        }
-    }
-
-    const questionIsAnswered =(sectionIndex: number, questionIndex: number): boolean=>{
-        let isAnswered = false
-        if (response.sections[sectionIndex] !== undefined){
-            if (response.sections[sectionIndex].answers[questionIndex]){
-                isAnswered = true
-            }
-        }
-        return isAnswered
     }
 
     const moveBackwards =()=>{
+        setReadyToGenerateReport(false)
         if (currentQuestionIndex > 0){
             setCurrentQuestionIndex(currentQuestionIndex - 1)
+            if (qnaSections !== undefined){
+                setCurrentAnswer(qnaSections[currentSectionIndex][currentQuestionIndex - 1].answer)
+            }
         } else if (currentSectionIndex > 0){
             setCurrentSectionIndex(currentSectionIndex - 1)
             setCurrentQuestionIndex(sections[currentSectionIndex - 1].questions.length - 1)
+            if (qnaSections !== undefined){
+                setCurrentAnswer(qnaSections[currentSectionIndex - 1][sections[currentSectionIndex - 1].questions.length - 1].answer)
+            }
         }
+    }
+
+    const generateReport =()=>{
+
     }
 
   return (
@@ -105,7 +114,6 @@ export default function CreateReport() {
                 <p className='text-white text-3xl'>
                     {`${currentQuestionIndex+1}. ${template.sections[currentSectionIndex].questions[currentQuestionIndex]}?`}
                 </p>
-                {/* {sections[currentSectionIndex].optional && <span className='text-white opacity-50'>optional</span>} */}
                 <textarea placeholder='answer the question'className='text-3xl bg-black focus:outline-none border-b-2 border-white text-white w-full mt-10'
                 rows={3} value={currentAnswer} onChange={(e)=>{setCurrentAnswer(e.target.value)}}/>
                 <div>
@@ -115,14 +123,16 @@ export default function CreateReport() {
             </div>
         ): (
             <div>
+                <input placeholder='name the report' className='text-3xl bg-black focus:outline-none border-b-2 border-white text-white w-full mt-10'
+                value={reportName} onChange={(e)=>{setReportName(e.target.value)}}/>
                 <button className='mt-10' >Generate report</button>
             </div>
         )}
         <img src={house} alt="template image" className='template-image object-cover '/>
-        {/* <div className='fixed bottom-0 end-0 bg-white mr-10 mb-5 flex p-5 rounded-xl'>
+        <div className='fixed bottom-0 end-0 bg-white mr-10 mb-5 flex p-5 rounded-xl'>
             <div className='mr-5' onClick={moveBackwards}><img src={arrowLeft} alt='left pointing arrow' /></div>
-            <div onClick={handleFowardArrowCLick}><img src={arrowRight} alt='right pointing arrow'/></div>
-        </div> */}
+            <div onClick={moveFoward}><img src={arrowRight} alt='right pointing arrow'/></div>
+        </div>
     </div>
   )
 }
